5a3bf4b0d71999ff3384123a120152795a7b2a05
Nguyen Duc Anh <anhnd@qsoft.com.vn>
2019-07-22 18:27:39 +0700

    selection p2

--- automationpractice.py
+++ automationpractice.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 import time
 import unittest
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import staleness_of
 class testCase (unittest.TestCase):
     def setUp(self):
         # create a new Chrome session
@@ -10,15 +12,32 @@ class testCase (unittest.TestCase):
         #self.driver.maximize_window()
         # navigate to an automationpractice
         self.driver.get("http://automationpractice.com/index.php")
+        self.old_page = "123"
 
     def selection(self):
+        #self.driver.implicitly_wait(15)
         self.element = self.driver.find_element_by_id("selectProductSort")
         self.drp = Select(self.element)
+       
         # select by visible text
         #self.drp.select_by_visible_text("Price: Lowest first") #lowest price
-
+        number_options = len(self.drp.options)
         #select by index
-        #self.drp.select_by_index(2) #Price highest
+        for i in range(number_options):
+            old_page = self.driver.find_element_by_tag_name('html')
+            self.element = self.driver.find_element_by_id("selectProductSort")
+            self.drp = Select(self.element)
+            self.drp.select_by_index(i) #Price highest
+
+        # self.element = self.driver.find_element_by_id("selectProductSort")
+        # self.drp = Select(self.element)    
+        # self.drp.select_by_index(3) 
+        # self.element = self.driver.find_element_by_id("selectProductSort")
+        # self.drp = Select(self.element)       
+        # self.drp.select_by_index(4).click()      
+        # self.drp.select_by_index(5).click()       
+        # self.drp.select_by_index(6).click()
+        
 
         #select by value 
         #self.drp.select_by_value("quantity:desc") # Stock
@@ -26,7 +45,6 @@ class testCase (unittest.TestCase):
         self.all_options = self.drp.options
         for option in self.all_options:
             print(option.text)
-
 
     def test_search_by_text(self):
         time.sleep(2)
@@ -46,6 +64,7 @@ class testCase (unittest.TestCase):
         self.assertEqual(1 , len(self.lists))  
         #self.assertIn(self.key_search,self.driver.title)
         self.selection()
+
          
     def tearDown(self):
         time.sleep(3)
